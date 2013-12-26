b3501caba9313500a7bff104af46b61ae708e406
Justin <justincyork@gmail.com>
2013-12-26 15:13:28 -0700

    Add conversion for Polylines and Polygons

--- src/to-geojson.js
+++ src/to-geojson.js
@@ -10,25 +10,98 @@
 
   google.maps.geojson.toGeoJSON = function(overlay){
     
-    var geoJson;
-    
-    // Switch on overlay type
+    // Markers
     if(overlay instanceof google.maps.Marker){
-      geoJson = _convertLatLng(overlay.getPosition());
+      return _convertMarker(overlay);
     }
-      
-    // Return resulting GeoJSON
-    return geoJson;
+    
+    // Polylines
+    else if(overlay instanceof google.maps.Polyline){
+      return _convertPolyline(overlay);      
+    }
+    
+    // Polygons
+    else if(overlay instanceof google.maps.Polygon){
+      return _convertPolygon(overlay);
+    }
+    
+    // Error catchall
+    else {
+      return _error("No conversion to GeoJSON is available for the given object type");
+    }
   };
   
   /**
-   * Convert a google.maps.LatLng into a GeoJSON Point
+   * Converts a google.maps.Polygon into a GeoJSON Polygon
    */
-  function _convertLatLng(latLng){
+  function _convertPolygon(polygon){
+    var rings = [];
+    
+    polygon.getPaths().forEach(function(path, i){
+      var ring = _convertPath(path);
+      
+      // The first and last point in a GeoJSON polygon ring 
+      // must be the same point 
+      ring.push(ring[0]);
+      
+      rings.push(ring);
+    });
+    
+    return {
+      "type": "Polygon",
+      "coordinates": rings
+    };
+  };
+  
+  /**
+   * Converts a google.maps.Polyline into a GeoJSON LineString
+   */
+  function _convertPolyline(polyline){
+    return {
+      "type": "LineString",
+      "coordinates": _convertPath(polyline.getPath())
+    };
+  };
+  
+  /**
+   * Converts a google.maps.Marker into a GeoJSON Point
+   */
+  function _convertMarker(marker){
     return {
       "type": "Point",
-      "coordinates": [latLng.lng(), latLng.lat()]
+      "coordinates": _convertLatLng(marker.getPosition())
     };
+  };
+  
+  /**
+   * Converts a Google Maps Path (MVCArray of LatLng) into
+   * an array of GeoJSON coordinates
+   */
+  function _convertPath(path){
+    var coords = [];
+    path.forEach(function(point, i){
+      coords.push(_convertLatLng(point));
+    });
+    return coords;
+  };
+  
+  /**
+   * Convert a google.maps.LatLng into a GeoJSON coordinate array
+   */
+  function _convertLatLng(latLng){
+    return [latLng.lng(), latLng.lat()];
+  };
+  
+  /**
+   * Formats an error object
+   */
+  var _error = function( message ){
+  
+    return {
+      type: "Error",
+      message: message
+    };
+  
   };
    
 }());
--- test/to.js
+++ test/to.js
@@ -1,8 +1,108 @@
-test("google.maps.Marker -> GeoJSON Point", function() {
+test("google.maps.Marker -> GeoJSON Point", function(){
   var marker = new google.maps.Marker({
     position: new google.maps.LatLng(53.45821, -13.195, true)
   });
   var geoJson = google.maps.geojson.toGeoJSON(marker);
   
   deepEqual(geoJson, {"type":"Point","coordinates":[-13.195,53.45821]}, "Passed!" );
+});
+
+test("google.maps.Polyline -> GeoJSON LineString", function(){
+  var polyline = new google.maps.Polyline({
+    path: [
+      new google.maps.LatLng(0, 0),
+      new google.maps.LatLng(1, 0),
+      new google.maps.LatLng(1, 1),
+      new google.maps.LatLng(0, 2)
+    ]
+  });
+  var convertedGeoJSON = google.maps.geojson.toGeoJSON(polyline);
+  var expectedGeoJSON = {
+    "type": "LineString",
+    "coordinates": [
+      [0, 0],
+      [0, 1],
+      [1, 1],
+      [2, 0]
+    ]
+  };
+  
+  deepEqual(convertedGeoJSON, expectedGeoJSON, "Passed!" );
+});
+
+test("google.maps.Polygon with one path -> GeoJSON Polygon", function(){
+  var polygon = new google.maps.Polygon({
+    paths: [
+      new google.maps.LatLng(0, 0),
+      new google.maps.LatLng(1, 0),
+      new google.maps.LatLng(1, 1),
+      new google.maps.LatLng(0, 1)
+    ]
+  });
+  var convertedGeoJSON = google.maps.geojson.toGeoJSON(polygon);
+  var expectedGeoJSON = {
+    "type": "Polygon",
+    "coordinates": [
+      [
+        [0, 0],
+        [0, 1],
+        [1, 1],
+        [1, 0],
+        [0, 0]
+      ]
+    ]
+  };
+  
+  deepEqual(convertedGeoJSON, expectedGeoJSON, "Passed!" );
+});
+
+test("google.maps.Polygon with two paths -> GeoJSON Polygon", function(){
+  var polygon = new google.maps.Polygon({
+    paths: [
+      [
+        new google.maps.LatLng(0, 0),
+        new google.maps.LatLng(1, 0),
+        new google.maps.LatLng(1, 1),
+        new google.maps.LatLng(0, 1)
+      ],
+      [
+        new google.maps.LatLng(.25, .25),
+        new google.maps.LatLng(.75, .25),
+        new google.maps.LatLng(.75, .75),
+        new google.maps.LatLng(.25, .75)
+      ]
+    ]
+  });
+  var convertedGeoJSON = google.maps.geojson.toGeoJSON(polygon);
+  var expectedGeoJSON = {
+    "type": "Polygon",
+    "coordinates": [
+      [
+        [0, 0],
+        [0, 1],
+        [1, 1],
+        [1, 0],
+        [0, 0]
+      ],
+      [
+        [0.25, 0.25],
+        [0.25, 0.75],
+        [0.75, 0.75],
+        [0.75, 0.25],
+        [0.25, 0.25]
+      ]
+    ]
+  };
+  
+  deepEqual(convertedGeoJSON, expectedGeoJSON, "Passed!" );
+});
+
+test("Unsupported google maps feature", function(){
+  var unsupported = new google.maps.Circle({
+    radius: 5,
+    center: new google.maps.LatLng(-1, 1)
+  });
+  var geoJson = google.maps.geojson.toGeoJSON(unsupported);
+  console.log(geoJson);
+  ok(geoJson.type === 'Error', "Detected an unsupported object type");
 });
