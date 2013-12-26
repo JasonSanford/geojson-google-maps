ae0ab6256e31eb667ec38db35a653f1f06d11785
Justin <justincyork@gmail.com>
2013-12-26 16:52:44 -0700

    Convert arrays over overlays

--- src/to-geojson.js
+++ src/to-geojson.js
@@ -8,8 +8,63 @@
     google.maps.geojson = {};
   }
 
-  google.maps.geojson.toGeoJSON = function(overlay){
+  google.maps.geojson.toGeoJSON = function(overlays){
     
+    // Array of overlays
+    if(_isArray(overlays)){
+      
+      // Convert each overlay to GeoJSON
+      var geoms = [];
+      for(var i = 0; i < overlays.length; i++){
+        geoms.push(_convertOverlay(overlays[i]));
+      }
+      
+      // Determine what type of GeoJSON collection should be returned
+      var multiPoint = true,
+          multiLineString = true,
+          multiPolygon = true;
+      for(var i = 0; i < geoms.length; i++){
+        var geo = geoms[i];
+        if(geo.type === "Point") {
+          multiLineString = false;
+          multiPolygon = false;
+        }
+        else if(geo.type === "LineString") {
+          multiPoint = false;
+          multiPolygon = false;
+        }
+        else if(geo.type === "Polygon") {
+          multiPoint = false;
+          multiLineString = false;
+        }
+      }
+      
+      // Convert to and return the propery GeoJSON collection type
+      if(multiPoint) {
+        return _createMultiPoint(geoms);
+      } else if(multiLineString){
+        return _createMultiLineString(geoms);
+      } else if(multiPolygon){
+        return _createMultiPolygon(geoms);
+      } else {
+        return {
+          "type": "GeometryCollection",
+          "geometries": geoms
+        };
+      }
+    }
+    
+    // Single overlay
+    else {
+      return _convertOverlay(overlays);
+    }
+    
+  };
+  
+  /**
+   * Convert one Google Maps overlay to GeoJSON
+   */
+  function _convertOverlay(overlay){
     // Markers
     if(overlay instanceof google.maps.Marker){
       return _convertMarker(overlay);
@@ -29,6 +84,49 @@
     else {
       return _error("No conversion to GeoJSON is available for the given object type");
     }
+  };
+  
+  /**
+   * Create a GeoJSON MultiPoint from an array of GeoJSON Points
+   */
+  function _createMultiPoint(points){
+    return {
+      "type": "MultiPoint",
+      "coordinates": _collapseCoordinates(points)
+    };
+  };
+  
+  /**
+   * Create a GeoJSON MultiLineString from an array of GeoJSON LineStrings
+   */
+  function _createMultiLineString(lines){
+    return {
+      "type": "MultiLineString",
+      "coordinates": _collapseCoordinates(lines)
+    };
+  };
+  
+  /**
+   * Create a GeoJSON MultiPolygon from an array of GeoJSON Polygons
+   */
+  function _createMultiPolygon(polygons){
+    return {
+      "type": "MultiPolygon",
+      "coordinates": _collapseCoordinates(polygons)
+    };
+  };
+  
+  /**
+   * Given an array of GeoJSON objects that have coordinates,
+   * extract the coordinate arrays and return them together
+   * in a single array.
+   */
+  function _collapseCoordinates(geos){
+    var coords = [];
+    for(var i = 0; i < geos.length; i++){
+      coords.push(geos[i].coordinates);
+    }
+    return coords;
   };
   
   /**
@@ -102,6 +200,14 @@
       message: message
     };
   
+  };
+  
+  /**
+   * Returns true if an object is an array
+   * http://perfectionkills.com/instanceof-considered-harmful-or-how-to-write-a-robust-isarray/
+   */
+  function _isArray(o) {
+    return Object.prototype.toString.call(o) === '[object Array]';
   };
    
 }());
--- test/to.js
+++ test/to.js
@@ -1,10 +1,34 @@
+module("Google Maps -> GeoJSON");
+
 test("google.maps.Marker -> GeoJSON Point", function(){
   var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(53.45821, -13.195, true)
+    position: new google.maps.LatLng(0, 1, true)
   });
-  var geoJson = google.maps.geojson.toGeoJSON(marker);
+  var convertedGeoJSON = google.maps.geojson.toGeoJSON(marker);
+  var expectedGeoJSON = {"type":"Point","coordinates":[1,0]};
   
-  deepEqual(geoJson, {"type":"Point","coordinates":[-13.195,53.45821]}, "Passed!" );
+  deepEqual(convertedGeoJSON, expectedGeoJSON, "Passed!" );
+});
+
+test("[google.maps.Marker] -> GeoJSON MultiPoint", function(){
+  var markers = [
+    new google.maps.Marker({
+      position: new google.maps.LatLng(0, 1, true)
+    }),
+    new google.maps.Marker({
+      position: new google.maps.LatLng(3, 5, true)
+    })
+  ];
+  var convertedGeoJSON = google.maps.geojson.toGeoJSON(markers);
+  var expectedGeoJSON = {
+    "type":"MultiPoint",
+    "coordinates": [
+      [1,0],
+      [5,3]
+    ]
+  };
+  
+  deepEqual(convertedGeoJSON, expectedGeoJSON, "Passed!" );
 });
 
 test("google.maps.Polyline -> GeoJSON LineString", function(){
@@ -24,6 +48,47 @@ test("google.maps.Polyline -> GeoJSON LineString", function(){
       [0, 1],
       [1, 1],
       [2, 0]
+    ]
+  };
+  
+  deepEqual(convertedGeoJSON, expectedGeoJSON, "Passed!" );
+});
+
+test("[google.maps.Polyline] -> GeoJSON LineString", function(){
+  var polylines = [
+    new google.maps.Polyline({
+      path: [
+        new google.maps.LatLng(0, 0),
+        new google.maps.LatLng(1, 0),
+        new google.maps.LatLng(1, 1),
+        new google.maps.LatLng(0, 2)
+      ]
+    }),
+    new google.maps.Polyline({
+      path: [
+        new google.maps.LatLng(0, -1),
+        new google.maps.LatLng(1, 2),
+        new google.maps.LatLng(4, 1),
+        new google.maps.LatLng(0, -2)
+      ]
+    })
+  ];
+  var convertedGeoJSON = google.maps.geojson.toGeoJSON(polylines);
+  var expectedGeoJSON = {
+    "type": "MultiLineString",
+    "coordinates": [
+      [
+        [0, 0],
+        [0, 1],
+        [1, 1],
+        [2, 0]
+      ],
+      [
+        [-1, 0],
+        [2, 1],
+        [1, 4],
+        [-2, 0]
+      ]
     ]
   };
   
@@ -91,6 +156,105 @@ test("google.maps.Polygon with two paths -> GeoJSON Polygon", function(){
         [0.75, 0.25],
         [0.25, 0.25]
       ]
+    ]
+  };
+  
+  deepEqual(convertedGeoJSON, expectedGeoJSON, "Passed!" );
+});
+
+test("[google.maps.Polygon] with one path -> GeoJSON Polygon", function(){
+  var polygons = [
+    new google.maps.Polygon({
+      paths: [
+        new google.maps.LatLng(0, 0),
+        new google.maps.LatLng(1, 0),
+        new google.maps.LatLng(1, 1),
+        new google.maps.LatLng(0, 1)
+      ]
+    }),
+    new google.maps.Polygon({
+      paths: [
+        new google.maps.LatLng(0, 0),
+        new google.maps.LatLng(-3, 0),
+        new google.maps.LatLng(5, 6),
+        new google.maps.LatLng(3, -1)
+      ]
+    })
+  ];
+  var convertedGeoJSON = google.maps.geojson.toGeoJSON(polygons);
+  var expectedGeoJSON = {
+    "type": "MultiPolygon",
+    "coordinates": [
+      [
+        [
+          [0, 0],
+          [0, 1],
+          [1, 1],
+          [1, 0],
+          [0, 0]
+        ]
+      ],
+      [
+        [
+          [0, 0],
+          [0, -3],
+          [6, 5],
+          [-1, 3],
+          [0, 0]
+        ]
+      ]
+    ]
+  };
+  
+  deepEqual(convertedGeoJSON, expectedGeoJSON, "Passed!" );
+});
+
+test("multiple overlays -> GeoJSON GeometryCollection", function(){
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(0, 1, true)
+  });
+  var polyline = new google.maps.Polyline({
+    path: [
+      new google.maps.LatLng(0, 0),
+      new google.maps.LatLng(1, 0),
+      new google.maps.LatLng(1, 1),
+      new google.maps.LatLng(0, 2)
+    ]
+  });
+  var polygon = new google.maps.Polygon({
+    paths: [
+      new google.maps.LatLng(0, 0),
+      new google.maps.LatLng(1, 0),
+      new google.maps.LatLng(1, 1),
+      new google.maps.LatLng(0, 1)
+    ]
+  });
+  var convertedGeoJSON = google.maps.geojson.toGeoJSON([marker, polyline, polygon]);
+  var expectedGeoJSON = {
+    "type": "GeometryCollection",
+    "geometries": [
+      {"type":"Point","coordinates":[1,0]},
+      {
+        "type": "LineString",
+        "coordinates": [
+          [0, 0],
+          [0, 1],
+          [1, 1],
+          [2, 0]
+        ]
+      },
+      {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [0, 0],
+            [0, 1],
+            [1, 1],
+            [1, 0],
+            [0, 0]
+          ]
+        ]
+      }
     ]
   };
   
